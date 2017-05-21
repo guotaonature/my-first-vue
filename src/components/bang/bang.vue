66a7a556da4c5c9df1cfae605018b048501680aa
Guo Tao <naturejolanthe@126.com>
2017-05-21 15:21:53 +0800

    学着别人--hy1115搭个最简单的webpack和静态数据的vue脚手架

---
+++ src/components/bang/bang.vue
@@ -0,0 +1,24 @@
+<script>
+    import Bang from './template/BangList.vue'
+    import { bangList } from 'static/data/data'
+    export default {
+        name: 'BangDan',
+        components: {
+            'app-bang': Bang
+        },
+        data() {
+            return {
+                bangList: bangList
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <ul class="bang-ul">
+            <app-bang v-for="list in bangList" :key="list.id" :list="list"></app-bang>
+        </ul>
+    </div>
+</template>
+
